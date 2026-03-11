c49e1a9c58fe9ad6fbdbf485815ced96c580a7ce
SuBroXz11 <subratkrp2@gmail.com>
2026-03-11 22:56:06 +0545

    feat: add AppNavigation with all screen routes

---
+++ app/src/main/java/subratk/com/np/AppNavigation.kt
@@ -0,0 +1,54 @@
+package subratk.com.np
+
+import androidx.compose.runtime.*
+import androidx.navigation.compose.*
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+
+        composable("login") {
+            LoginScreen(
+                onRegisterClick = { navController.navigate("register") }
+            )
+        }
+
+        composable("register") {
+            RegisterScreen(
+                onRegisterClick = { navController.navigate("home") },
+                onLoginClick = { navController.navigate("login") }
+            )
+        }
+
+        composable("home") {
+            TodoListScreen(
+                onAddClick = { navController.navigate("addtodo") },
+                onEditClick = { navController.navigate("edittodo") },
+                onProfileClick = { navController.navigate("profile") }
+            )
+        }
+
+        composable("addtodo") {
+            AddEditTodoScreen(
+                isEditing = false,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+
+        composable("edittodo") {
+            AddEditTodoScreen(
+                isEditing = true,
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+
+        composable("profile") {
+            ProfileScreen(
+                onBackClick = { navController.popBackStack() }
+            )
+        }
+
+    }
+}
